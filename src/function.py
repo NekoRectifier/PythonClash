8375ec28025b1473bd051ef03c88da5f84919ee1
NekoRectifier <hyx123250@gmail.com>
2023-12-25 03:24:13 +0800

    temp

--- src/function.py
+++ src/function.py
@@ -2,14 +2,14 @@ import utils
 from loguru import logger
 import os
 
-def setup():
+def setup(_dir):
     shell_type = utils.get_shell_type()
     if shell_type == 'Fish':
         _path = "/home/" + utils.get_curr_username() + "/.config/fish/config.fish"
         _config_valid = os.path.exists(_path) 
 
         if _config_valid and not utils.check_string_in_file(_path, "PythonClash"):
-            utils.append_file("source " + os.path.dirname(__file__) + "/../scripts/function.fish", _path)
+            utils.append_file("source " + _dir + "/scripts/PythonClash.fish", _path)
             logger.info("adding function to shell config file...")
         elif _config_valid and utils.check_string_in_file(_path, "PythonClash"):
             logger.warning("functions had been added to the shell config file!")
@@ -17,6 +17,4 @@ def setup():
             logger.error("config file is not in default pos")
     else:
         logger.error("not supported to set shell functions.")
-
-def update():
 
--- src/main.py
+++ src/main.py
@@ -1,22 +1,55 @@
 import function
 import argparse
+from loguru import logger
+import json
+import os
 
 parser = argparse.ArgumentParser(usage="\n\tpython3 main.py {setup, update, start, stop, restart}\n")
 
 parser.add_argument('function')
-
+parser.add_argument('-u', '--url')
 args = parser.parse_args()
+
+exec_dir = os.path.dirname(__file__).rpartition("/")[0]
+_conf_path = exec_dir + "/conf/conf.json"
+
+conf = {}
+
+if not os.path.exists(_conf_path):
+    with open(_conf_path, 'w') as f:
+        f.write("{}")
+    f.close()
+else:
+    with open(_conf_path, 'w+') as f:
+        _raw = f.read()
+        if _raw != "":
+            conf = json.loads(_raw)
+        else:
+            logger.debug('conf.json is empty')
+            f.write("{}")
+    f.close()
 
 
 if __name__ == "__main__":
-    input_func = args.function 
+    input_func = args.function
+
+    if args.url is not None:
+        logger.info("subscribe url updated...")
+        conf['sub_url'] = args.url
 
     if input_func == 'update':
         print("starting update now...")
         function.update()
     elif input_func == 'setup':
-        function.setup()
+        function.setup(exec_dir)
     elif input_func == 'start':
         print("start clash core")
     else:
         print('usage')
+
+    # saving conf
+    with open(_conf_path, 'w') as cf:
+        json.dump(conf, cf)
+        
+
+# catch when program exits, to save conf to file
